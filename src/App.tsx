5e238687be54dfa393bc2d7aabe7b5f9aae9c9a5
Luis Filipe Pedroso <luis_filipe42@outlook.com>
2022-05-11 15:35:26 -0300

    feat: create initial structure of the project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Button from "./components/Button";
+import { Heading } from "./components/Text";
+import Home from "./pages/Home";
+
+function App() {
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  )
+}
+
+export default App;
