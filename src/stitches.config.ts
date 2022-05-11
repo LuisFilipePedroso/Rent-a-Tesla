2f2b2e3bb291bc1fd856fd6c83366240265d696a
Luis Filipe Pedroso <luis_filipe42@outlook.com>
2022-05-11 15:36:02 -0300

    feat: create stitches config

---
+++ src/stitches.config.ts
@@ -0,0 +1,18 @@
+import { createStitches } from "@stitches/react";
+
+export const {
+  styled,
+} = createStitches({
+  theme: {
+    colors: {
+      gray50: '#fff',
+      gray100: '#e5e5e5',
+      gray200: '#ccc',
+      gray300: '#b2b2b2',
+      gray400: '#999999',
+      gray500: '#7f7f7f',
+      gray600: '#323232',
+      primary: '#1A4AF8',
+    }
+  }
+})
