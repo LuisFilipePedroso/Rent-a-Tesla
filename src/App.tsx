f8bde8e27f178add9b56f980bd7edbaaf586cbb9
Luis Filipe Pedroso <luis_filipe42@outlook.com>
2022-05-11 15:36:57 -0300

    feat: import Home to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
-import Button from "./components/Button";
-import { Heading } from "./components/Text";
-import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      Ola
     </div>
   )
 }
