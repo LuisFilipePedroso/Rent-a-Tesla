508d7098dae15c84c394cd88d3d9cb399135ac48
Luis Filipe Pedroso <luis_filipe42@outlook.com>
2022-05-11 15:36:14 -0300

    feat: create Button component

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,15 @@
+import { styled } from "../../stitches.config";
+
+const Button = styled('button', {
+  backgroundColor: '$primary',
+  color: '$gray50',
+  padding: '8px 32px',
+  borderRadius: 8,
+  fontSize: 18,
+  border: 'none',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+})
+
+export default Button;
