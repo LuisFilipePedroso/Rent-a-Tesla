d83bbc4d403ed28df9265c2a3ac9db1e62d604c9
Luis Filipe Pedroso <luis_filipe42@outlook.com>
2022-05-13 14:32:36 -0300

    feat: create primary and secondary variant

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,15 +1,31 @@
 import { styled } from "../../stitches.config";
 
 const Button = styled('button', {
-  backgroundColor: '$primary',
-  color: '$gray50',
   padding: '8px 32px',
   borderRadius: 8,
-  fontSize: 18,
+  fontSize: "1rem",
+  fontWeight: "bold",
   border: 'none',
   display: 'flex',
   alignItems: 'center',
   justifyContent: 'center',
+
+  variants: {
+    variant: {
+      primary: {
+        backgroundColor: '$primary',
+        color: '$gray50',
+      },
+      secondary: {
+        backgroundColor: '$gray50',
+        color: '$primary',
+      }
+    }
+  },
+
+  defaultVariants: {
+    variant: "primary"
+  }
 })
 
 export default Button;
