ade804bc008f43d4faf2c7484a18286ad674fe4a
Luis Filipe Pedroso <luis_filipe42@outlook.com>
2022-05-13 15:22:12 -0300

    fix: add menu

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,12 @@
 import Header from "../../components/Header";
+import Menu from "../../components/Menu";
 import { Container } from "./styles";
 
 function Home() {
 
   return (
     <Container>
+      <Menu />
       <Header />
     </Container>
   )
