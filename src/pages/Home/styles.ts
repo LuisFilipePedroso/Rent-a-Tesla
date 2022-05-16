7b1fecf0c1143c0999b2045b4e0f08a94b808d2e
Luis Filipe Pedroso <luis_filipe42@outlook.com>
2022-05-16 17:09:15 -0300

    fix: add missing padding

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,7 +1,10 @@
 import { styled } from "../../stitches.config";
+import { pxToRem } from "../../utils";
 
 export const Container = styled("div", {
   display: "flex",
   flexDirection: "column",
   width: "100%",
+  paddingTop: `${pxToRem("16")}rem`,
+  paddingBottom: `${pxToRem("16")}rem`,
 });
