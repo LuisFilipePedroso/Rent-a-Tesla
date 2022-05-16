7e26df3a79fc71e2bb185a75985541db7a945ced
Luis Filipe Pedroso <luis_filipe42@outlook.com>
2022-05-16 17:07:11 -0300

    feat: create content component

---
+++ src/components/Content/index.tsx
@@ -0,0 +1,9 @@
+import { styled } from "../../stitches.config";
+
+export default styled("div", {
+  display: "flex",
+  width: "100%",
+  maxWidth: 1480,
+  marginLeft: "auto",
+  marginRight: "auto",
+});
